d181d80f52686315cd19f0100858975ae630a780
Uysim <uysimty@gmail.com>
2017-06-07 10:48:31 +0700

    add edit and delete button to category show page

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,7 +4,27 @@
     <div class="col-xs-12 col-md-6 col-md-offset-3">
       <h1>{{ $category->name }}</h1>
       <p>{{ $category->description }}</p>
-      <a href="{!! route('categories.index') !!}" class="btn btn-default">Back</a>
+      <a
+        href="{!! route('categories.index') !!}"
+        class="btn btn-default">
+        <i class="fa fa-caret-left"></i>
+        Back
+      </a>
+      <a
+        href="{!! route('categories.edit', ['category'=>$category]) !!}"
+        class="btn btn-primary">
+        <i class="fa fa-pencil-square-o"></i>
+        Edit
+      </a>
+      <a href="{!! route('categories.destroy', ['category'=>$category]) !!}"
+          data-method="delete"
+          data-token="{{csrf_token()}}"
+          data-confirm="Are you sure?"
+          class='btn btn-danger'>
+        <i class="fa fa-trash"></i>
+        Destroy
+      </a>
+
     </div>
   </div>
 
